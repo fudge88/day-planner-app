a0e6073f7e8bc3973c4641023fc06aafed38dcfb
Fahra Akhlaq <f.akhlaq@outlook.com>
2021-10-09 18:32:48 +0100

    used bootstrap grid to create planner

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,6 @@
-console.log("hello");
+console.log(moment().format());
+console.log(moment().startOf("hour").fromNow());
+
+$("#currentDay").text(now.format("dddd MMMM DD, YYYY"));
+
+$(document).ready(onReady);
