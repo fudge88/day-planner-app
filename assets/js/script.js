7250bb8ac29ad65707612bb9cc3192f4617a7523
Fahra Akhlaq <f.akhlaq@outlook.com>
2021-10-13 13:39:35 +0100

    moment js implemented

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,71 @@
-console.log(moment().format());
-console.log(moment().startOf("hour").fromNow());
+const timeBlockArray = [
+  {
+    label: "9",
+    key: 9,
+  },
+  {
+    label: "10",
+    key: 10,
+  },
+  {
+    label: "11",
+    key: 11,
+  },
+  {
+    label: "12",
+    key: 12,
+  },
+  {
+    label: "13",
+    key: 13,
+  },
+  {
+    label: "14",
+    key: 14,
+  },
+  {
+    label: "15",
+    key: 15,
+  },
+  {
+    label: "16",
+    key: 16,
+  },
+  {
+    label: "17",
+    key: 17,
+  },
+];
 
-$("#currentDay").text(now.format("dddd MMMM DD, YYYY"));
+// onLoad
+
+// get current day
+
+// render current day
+
+// render time-blocks using map (construct time block)or a for each
+
+// get text from object
+// if undefined - theres no value
+// if present- true- class red
+// if future - true -class green
+
+const onSave = function () {
+  // get time block btn was clicked
+  // event delegation (target)
+  // get text from text area .value
+  // take text and key for time and write to LS
+};
+
+const currentDayTime = $("#currentDay");
+const onReady = function () {
+  const timerTick = function () {
+    const dateTime = moment();
+    const dateTimeFormat = dateTime.format("dddd DD MMMM, YYYY kk:mm");
+
+    currentDayTime.text(dateTimeFormat);
+  };
+  const timer = setInterval(timerTick, 1000);
+};
 
 $(document).ready(onReady);
